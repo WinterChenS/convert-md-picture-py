1a2554568f66a27fbc4839b5855ba375456e2521
WinterChenS <1085143002@qq.com>
2021-08-04 11:20:48 +0800

    done

--- start.py
+++ start.py
@@ -3,11 +3,19 @@
 
 import os
 import re
-#D:\private\study\project\blog-back\source\_posts
-g = os.walk(r"/Users/winterchen/IdeaProjects/blog-back/source/_posts")
+import urllib.request
+import string
+from urllib.parse import quote
+import time
 
 
-reg = re.compile('http?:*.*.(?:jpg|png)', re.S)
+#/Users/winterchen/IdeaProjects/blog-back/source/_posts
+g = os.walk(r"D:\private\study\project\blog-back\source\_posts")
+
+
+regex = r"(http.*?jpg)"
+local_path = "D:\\private\\study\\project\\img\\posts\\"
+new_host_name_prefix = r"https://cdn.jsdelivr.net/gh/WinterChenS/img/posts/"
 
 
 def select_md_file_from_dir():
@@ -24,19 +32,35 @@ def select_md_file_from_dir():
 
 def read_md_file(file_path, file_name):
     if(os.path.splitext(file_name)[-1] == ".md" or os.path.splitext(file_name)[-1] == ".json"):
+        new_data = ''
         print("this is markdown file or json file")
-        with open(file_path, 'r+', encoding='utf-8') as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            
             for line in f.readlines():
                 print(line)
-                if re.match(reg, line):
-                            m1 = reg.findall(line)
-                            print(m1[0])
+                images = re.findall(regex, line)
+                if len(images) == 0:
+                    print("not match")
                 else:
-                            print("data Not match .")
-
-
-
-
+                    try:
+                        url = images[0]
+                        url = quote(url, safe=string.printable)
+                        print("提取到图片地址：" + url)                
+                        filename = os.path.basename(url)
+                        print(filename)
+                        t = time.time()
+                        name = str(int(round(t * 1000000))) + ".jpg"
+                        local_path_filename = local_path + name 
+                        # download resource to local
+                        urllib.request.urlretrieve(url, local_path_filename)
+                        new_url = new_host_name_prefix + name
+                        print("新地址为: " + new_url)
+                        line = re.sub(url, new_url, line)
+                    except Exception as e:
+                        print("报错的文件：" + file_path + "报错的行：" + line)
+                new_data += line
+        with open(file_path, 'w', encoding='utf-8') as f2:
+            f2.write(new_data)
 def run():
     select_md_file_from_dir()
     
--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 import os
 import re
 
-regex = r"(http.*?jpg)"
+regex = r".*(http.*?jpg)"
 line = "http://img.winterchen.com/simon-abrams-286276-unsplash.jpg\n"
 res = re.match(regex, line)
 print("提取到图片地址：" + res.group(1))
